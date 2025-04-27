c9411e33197f786adb96c6083a868890a64ba8ad
liushy <l.shynkewich@gmail.com>
2025-04-27 21:19:52 +0300

    update setValidFileName method

--- src/main/java/org/example/utils/SearchResultsFileWriter.java
+++ src/main/java/org/example/utils/SearchResultsFileWriter.java
@@ -3,6 +3,8 @@ package org.example.utils;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Scanner;
 
@@ -10,14 +12,10 @@ public class SearchResultsFileWriter {
 
     private static Path resultDir;
 
-//    choose path for file
-//    public static void chooseResultFileDirectory() {
-//        System.out.println("Input directory to save file: ");
-//        String writePath = new Scanner(System.in).nextLine();
-//    }
-
     public static Path setValidFileName(String fileName) {
-        return Path.of(fileName.replaceAll("[^a-zA-Z0-9]", "_") + ".txt");
+        String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd--HH-mm-ss"));
+
+        return Path.of(fileName.replaceAll("[^a-zA-Z0-9]", "_") + "_" + timestamp + ".txt");
     }
 
     public static Path getResultDir() throws IOException {
@@ -30,7 +28,29 @@ public class SearchResultsFileWriter {
         return resultDir;
     }
 
-    /*
+    public static void createResultFile(String fileName, List<String> searchResults) {
+        try {
+
+        Path directory = getResultDir();
+        Path resultFile = directory.resolve(setValidFileName(fileName));
+
+            Files.createFile(resultFile);
+            Files.write(resultFile, searchResults);
+
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+            System.out.println("No results!");
+        }
+    }
+}
+
+//    choose path for file
+//    public static void chooseResultFileDirectory() {
+//        System.out.println("Input directory to save file: ");
+//        String writePath = new Scanner(System.in).nextLine();
+//    }
+
+/*
     public static Path createDirectory() throws IOException {
         if (Files.exists(resultDir)) {
             if (Files.isDirectory(resultDir)) {
@@ -41,19 +61,3 @@ public class SearchResultsFileWriter {
         } return resultDir;
     }
     */
-
-
-    public static void createResultFile(String fileName, List<String> searchResults) {
-        try {
-        Path directory = getResultDir();
-        Path directoryWithFile = directory.resolve(setValidFileName(fileName));
-
-            Files.createFile(directoryWithFile);
-            Files.write(directoryWithFile, searchResults);
-        } catch (IOException e) {
-            System.out.println("Error: " + e.getMessage());
-            System.out.println("No results!");
-        }
-    }
-}
-
